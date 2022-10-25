e8d6aa07d42b6ea982f5653fb8431bbfb9b5001b
SongKiHo <project2056@naver.com>
2022-10-25 22:40:09 +0900

    Changed Development constant name and type

--- src/Loader.php
+++ src/Loader.php
@@ -11,10 +11,10 @@ use skh6075\pmexpansion\expansion\EntityExpansion;
 use skh6075\pmexpansion\expansion\ItemExpansion;
 
 final class Loader extends PluginBase{
-	public const VERSION_TYPE = "Development";
+	public const RELEASED = false;
 
 	protected function onEnable() : void{
-		if(self::VERSION_TYPE === "Development"){
+		if(!self::RELEASED){
 			$this->getLogger()->warning("The system version you are using is the one in development.");
 			$this->getLogger()->warning("We are not responsible for any problems that arise when using this version.");
 			$this->getLogger()->warning("Please use the latest version that has been developed. Link: https://github.com/skh6075/PMExpansion/tree/pm5");
