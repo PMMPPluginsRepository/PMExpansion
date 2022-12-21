26aa24730be41080daa43c773461dce6261aee35
SongKiHo <project2056@naver.com>
2022-12-21 19:43:25 +0900

    Added Campfire & SoulCampfire

--- src/item/ExtraVanillaItems.php
+++ src/item/ExtraVanillaItems.php
@@ -15,6 +15,8 @@ use pocketmine\utils\CloningRegistryTrait;
  * @method static Kelp KELP()
  * @method static Chain CHAIN()
  * @method static Camera CAMERA()
+ * @method static Campfire CAMPFIRE()
+ * @method static SoulCampfire SOUL_CAMPFIRE()
  */
 
 final class ExtraVanillaItems{
@@ -39,5 +41,7 @@ final class ExtraVanillaItems{
 		self::register('kelp', new Kelp(new IID(Kelp::getFixedTypeId()), "Kelp"));
 		self::register('chain', new Chain(new IID(Chain::getFixedTypeId()), 'Chain'));
 		self::register('camera', new Camera(new IID(Camera::getFixedTypeId()), 'Camera'));
+		self::register('campfire', new Campfire(new IID(Campfire::getFixedTypeId()), 'Campfire'));
+		self::register('soul_campfire', new SoulCampfire(new IID(SoulCampfire::getFixedTypeId()), 'Soul Campfire'));
 	}
 }
---
+++ src/item/SoulCampfire.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace skh6075\pmexpansion\item;
+
+use pocketmine\block\Block;
+use pocketmine\item\Item;
+use skh6075\pmexpansion\block\ExtraVanillaBlocks;
+use skh6075\pmexpansion\item\utils\ItemTypeIdTrait;
+
+class SoulCampfire extends Item{
+	use ItemTypeIdTrait;
+
+	public function getBlock(?int $clickedFace = null) : Block{
+		return ExtraVanillaBlocks::SOUL_CAMPFIRE();
+	}
+}
